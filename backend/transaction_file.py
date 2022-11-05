627ff8fb27875732ce1e470d25e7b90b510c2064
Abhishek Kumar <akumar58@buffalo.edu>
2022-11-05 14:46:31 -0400

    --add insert user function

--- backend/transaction_file.py
+++ backend/transaction_file.py
@@ -1,3 +1,21 @@
 import sqlite3
+import random
 
-class 
+class NewsDatabase:
+    def __init__(self):
+        self.conn = sqlite3.connect('../data/news.db', check_same_thread=False)
+        
+    
+    def insert_user(self,data):
+        c = self.conn.cursor()
+        #extract user data
+        # user_id = data["user_id"]
+        pub_addr = data["public_addr"]
+        name = data["name"]
+        email = data["email"]
+        balance = data['balance']
+
+        c.execute("INSERT INTO user(public_addr, name, email, balance) VALUES(?,?,?,?)", (pub_addr, name, email, balance))
+        print("User data inserted in database")
+        self.conn.commit()
+        # self.conn.close()
